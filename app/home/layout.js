990b18e8ceb7c216d04c84b35f3ebe4c408a7207
uibymaverick <uibymaverick@gmail.com>
2024-09-26 18:37:40 +0400

    connect wallet page for not connected state

--- app/home/layout.js
+++ app/home/layout.js
@@ -3,14 +3,21 @@ import React from 'react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { WatchlistProvider } from '@/context/WatchlistContext';
+import { useAccount } from 'wagmi';
+import ConnectWallet from '@/components/ConnectWallet';
 
 function layout({ children }) {
+  const { isConnected } = useAccount()
   return (
     <WatchlistProvider>
       <main className={`flex flex-col min-h-dvh pb-12`}>
         <Header />
         <main className='container flex-grow py-4 pl-4 pr-4 mx-auto'>
-          {children}
+          {isConnected ? children : <div className='flex flex-col items-center justify-center h-full gap-4'>
+            <h1 className='text-2xl font-bold '>Please connect your wallet</h1>
+            <p className='text-muted-foreground'> You need to connect your wallet to use this app</p>
+            <ConnectWallet />
+          </div>}
         </main>
         <Footer />
       </main>
